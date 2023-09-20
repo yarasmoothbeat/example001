bf48ca6934fa332f4d10d606164f892ba4c383e4
master <yarasmoothbeat@gmail.com>
2023-09-20 13:47:32 +0500

    fail

---
+++ Example011/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 13, 22, 3, 42, 54, 66, 37, 18, };
+int n = array.Lenght;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(array[index]);
+        break;
+    }
+    index = index++;
+}

ca58590c794e9a045c668cc1a34262ae8013625a
master <yarasmoothbeat@gmail.com>
2023-09-20 14:17:17 +0500

    fail 2

---
+++ Example012/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Lenght;
+    int index = 0;
+    while(index>lenght)
+    {
+        collection[index] = new Random() .Next (1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Lenght;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine (col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+
